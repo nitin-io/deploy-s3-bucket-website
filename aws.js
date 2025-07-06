192dbf7e74caa814573db4523736d21a3a31cbf4
Nitin Chaudhary <nitin.io@outlook.com>
2025-07-06 12:21:23 +0530

    remove hard coded variables

--- aws.js
+++ aws.js
@@ -10,10 +10,10 @@ import { fileURLToPath } from "node:url";
 import { existsSync } from "node:fs";
 
 // CONSTANTS
-const region = process.env.AWS_REGION || "ap-south-1";
+const region = process.env.AWS_REGION;
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-const bucket = process.env.AWS_S3_BUCKET || "nitinchaudhary.dev";
+const bucket = process.env.AWS_S3_BUCKET;
 const options = { region, bucket };
 
 if (accessKeyId && secretAccessKey) {
